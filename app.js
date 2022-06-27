027c3ca41268e3b2af35776905e45cc10ae34150
Jeremy Hutchings <79938993+jhutchings99@users.noreply.github.com>
2022-06-27 11:59:04 -0600

    set up webstore page

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const API_URL = "https://fakestoreapi.com";
 
+Vue.component("product", {
+    props: ["item", "cart"],
+    template:`
+    <div class="item">
+        <img :src="item.image">
+        <div class="item-info">
+            <h1> {{ item.title }} </h1>
+            <p> {{ item.description }} </p>
+            <i> \${{ item.price }} </i>
+            <button @click="addToCart()" class="add-cart">Add to cart</button>
+        </div>
+    </div>
+    `,
+    methods: {
+        addToCart: function () {
+            this.cart.push(this.item);
+        }
+    }
+})
+
 var app = new Vue({
     el: "#app",
     data: {
         products: [],
+        cart: [],
         currentPage: 1,
     },
     created: async function () {
