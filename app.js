127ab3d292c7ab2565a0e7d81b8b695beb271768
Jeremy Hutchings <79938993+jhutchings99@users.noreply.github.com>
2022-06-27 12:33:07 -0600

    added option to go from cart to webstore and added option to remove item from cart

--- app.js
+++ app.js
@@ -20,16 +20,39 @@ Vue.component("product", {
     }
 })
 
+Vue.component("cart-product", {
+    props: ["item", "cart"],
+    template:`
+    <div class="item">
+    <img :src="item.image">
+        <div class="item-info">
+            <h1> {{ item.title }} </h1>
+            <p> {{ item.description }} </p>
+            <i> \${{ item.price }} </i>
+            <button @click="removeFromCart()" class="add-cart">Remove from cart</button>
+        </div>
+    </div>
+    `,
+    methods: {
+        removeFromCart: function () {
+            this.cart.splice(this.cart.indexOf(this.item), 1)
+        }
+    }
+})
+
 var app = new Vue({
     el: "#app",
     data: {
         products: [],
         cart: [],
         currentPage: 1,
+        cartTotal: 0.00,
     },
     created: async function () {
         const response = await fetch(API_URL + "/products")
         const data = await response.json();
         this.products = data;
+    },
+    methods: {
     }
 });
